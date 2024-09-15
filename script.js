eaae7979fbfd8f0674a71e0a71aed92d3667ba52
CosmoNotFound <165526939+CosmoNotFound@users.noreply.github.com>
2024-09-15 19:58:09 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var ambientSound = document.getElementById('ambient-sound');
+    ambientSound.volume = 0.2; // Low volume for subtle background noise
+    ambientSound.play();
+});
